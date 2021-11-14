db96b53381d528aa58540e98dbcfaa7c443565ba
SaxenaKuhoo <92252901+SaxenaKuhoo@users.noreply.github.com>
2021-11-14 23:45:45 +0530

    Exception Handling in Array

--- Assign2.java
+++ Assign2.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Assign2 {
     public static void main(String[] args) {
         int n;
-        int a[] = new int[2];
         Scanner s = new Scanner(System.in);
         System.out.println("Enter size of array");
         n = s.nextInt();
+        int a[] = new int[2];
         System.out.println("Enter elements of array");
         for (int i = 0; i < n; i++) {
             try
