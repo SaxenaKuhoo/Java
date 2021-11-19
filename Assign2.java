e6fabf1cc0213cdad98bbb7d59d1c84e2405d639
SaxenaKuhoo <92252901+SaxenaKuhoo@users.noreply.github.com>
2021-11-19 14:28:23 +0530

    Exception handling in array elements

--- Assign2.java
+++ Assign2.java
@@ -1,31 +1,37 @@
 import java.util.Scanner;
 
-public class Assign2 {
+class Assign2 {
     public static void main(String[] args) {
         int n;
+        int f = 0;
+
         Scanner s = new Scanner(System.in);
         System.out.println("Enter size of array");
         n = s.nextInt();
-        int a[] = new int[2];
+        int a[] = new int[n];
         System.out.println("Enter elements of array");
         for (int i = 0; i < n; i++) {
-            try
-            {
-            a[i] = s.nextInt();
-            }
-            catch(Exception e)
-            {
+            try {
+                // a[i] = s.nextInt();
+                String b = s.next();
+                a[i] = Integer.parseInt(b);
+            } catch (Exception e) {
                 System.out.println(e);
+                f = 1;
             }
         }
-        System.out.println("Enetr index");
-        int i;
-        i = s.nextInt();
-        try {
-            System.out.println(a[i]);
-            System.out.println("Succsessful");
-        } catch (Exception e) {
-            System.out.println(e);
+        if (f == 0) {
+            System.out.println("Enetr index");
+            int i;
+            try {
+                i = s.nextInt();
+                System.out.println(a[i]);
+                System.out.println("Succsessful");
+            } catch (Exception e) {
+                System.out.println(e);
+            } finally {
+                s.close();
+            }
         }
     }
 }
